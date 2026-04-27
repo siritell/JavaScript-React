911bad6c73f28312d5b753c9ac28e9d0e4d27c14
siritell <siri.tell@hotmail.com>
2026-04-27 16:25:26 +0200

    Update seed data with full review text

--- Assignment 2/project/server/api.js
+++ Assignment 2/project/server/api.js
@@ -43,14 +43,14 @@ app.get("/seed", async (req, res) => {
       name: "Miyakodori",
       address: "Upplandsgatan 7",
       image: "Miyakodori.jpg",
-      text: "Miyakodori is a vibrant yet relaxed Japanese izakaya...",
+      text: "Miyakodori is a vibrant yet relaxed Japanese izakaya. You can pick your favourites from the menu, choose a special from their black board, or leave the choice to your server and go for the omakase. They have a separate bar as well, where you can enjoy drinks as well as eat.",
       standouts: ["pork belly skewers", "chicken liver mousse with milk bread"],
     },
     {
       name: "Lu",
       address: "Skånegatan 88",
       image: "Lu.jpg",
-      text: "Lu serves Cantonese street style food from Hong Kong...",
+      text: "Lu serves Cantonese street style food from Hong Kong, in a chaotic yet cosy, canteen like setting. If you are fortunate to live (somewhat) close by, they also offer take away, which means you can enjoy their food even if it's packed.",
       standouts: ["pork dumplings", "beef noodles"],
     },
   ]);
